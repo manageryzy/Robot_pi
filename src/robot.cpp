0c275f9189655120f8154a66502c0632b455e247
manageryzy <manageryzy@live.cn>
2014-10-23 16:49:35 +0800

    test Signed-off-by: manageryzy <manageryzy@live.cn>

--- src/robot.cpp
+++ src/robot.cpp
@@ -5,6 +5,7 @@
 #include "serialOut.h"
 #include "opencv.h"
 #include "confReader.h"
+#include <pthread.h>
 
 
 int main(int argc, char** argv )
@@ -18,13 +19,15 @@ int main(int argc, char** argv )
 	sequencer = new ASDFrameSequencerWebCam();
         (dynamic_cast<ASDFrameSequencerWebCam*>(sequencer))->open(-1);
 
-        if (! sequencer->isOpen())
-        {
-		std::cout << std::endl << "Error: Cannot initialize the default Webcam" << std::endl << std::endl;
-		return -1;
-        }
-	
+	if (! sequencer->isOpen())
+	{
+	std::cout << std::endl << "Error: Cannot initialize the default Webcam" << std::endl << std::endl;
+	return -1;
+	}
+
+#ifdef SHOW_GUI
 	cvNamedWindow("capture", CV_WINDOW_AUTOSIZE);
+#endif
 	
 	while ((img = sequencer->getNextImage()) != 0)
 	{
@@ -33,10 +36,15 @@ int main(int argc, char** argv )
 		img_canny =cvCreateImage(cvGetSize(img),img->depth,1);
 		cvCvtColor(img,img_gray,CV_RGB2GRAY);
 		cvCanny(img_gray,img_canny,30,50,3);
+
+#ifdef SHOW_GUI
 		cvShowImage ("capture", img_canny);
+#endif
+
 		cvReleaseImage(&img);
 		cvReleaseImage(&img_gray);
 		cvReleaseImage(&img_canny);
+		sleep(5000);
 		if (cvWaitKey(1) == 27)
 			break;
 	}
@@ -45,8 +53,9 @@ int main(int argc, char** argv )
 	delete sequencer;
 
 	//cvReleaseImage(&filterMask);
-
+#ifdef SHOW_GUI
 	cvDestroyWindow("capture");
+#endif
 	
 	return 0;
 }
